662d6a54df3417ced87605f959c40c74d7c7785c
gempro <gempro@gem2021testbeam.cern.ch>
2022-09-04 16:34:28 +0200

    Add rate capability plotting

--- analysis/calibration/ratecapability.py
+++ analysis/calibration/ratecapability.py
@@ -13,6 +13,7 @@ import mplhep
 mplhep.set_style("ROOT")
 
 saturating_curve = lambda x, A, tau: (A*x) / (1 + tau*(A*x))
+ratecapability_curve = lambda rate, tau: 1 / (1 + tau*rate)
 
 def main():
 
@@ -53,7 +54,9 @@ def main():
     vfat_index = dict(vfats)
     vfat_index = { v: k for k, v in vfat_index.items()}
 
-    def plot_rate(vfat_df):
+    def analyze_rate(vfat_df):
+        """ Plot rate vs source power, fit and calculate efficiency: """
+
         vfat = vfat_df["vfat"].iloc[0]
         iax = vfat_index[vfat]
         
@@ -62,31 +65,51 @@ def main():
         popt = [9e4, 0]
         [a, tau], pcov = curve_fit(saturating_curve, flux, rate, popt, err_rate)
         err_a, err_tau = np.sqrt(np.diag(pcov))
+        true_rate, err_true_rate = a*flux, err_a*flux
+
+        efficiency = rate / true_rate
+        err_efficiency = efficiency * np.sqrt( err_rate**2/rate**2 + err_true_rate**2/true_rate**2 )
 
         if args.verbose: print("a={}, tau={}".format(a, tau))
 
-        ax[iax].errorbar(flux, rate/1e6, yerr=err_rate/1e6, fmt=".", color="black")
-        ax[iax].plot(
+        rate_ax[iax].errorbar(flux, rate/1e6, yerr=err_rate/1e6, fmt=".", color="black")
+        rate_ax[iax].plot(
             flux, saturating_curve(flux, a, tau)/1e6, "-",
             color="red", label=f"$\\tau\,=\,{tau*1e9:1.2f}\,\pm{err_tau*1e9:1.2f}\,ns$"
         )
-        ax[iax].legend()
-        ax[iax].set_xlabel("X-ray current (µA)")
-        ax[iax].set_title("VFAT {}".format(vfat))
-        ax[iax].set_ylabel("Interacting particle rate (MHz/strip)")
-            
+        rate_ax[iax].legend()
+        rate_ax[iax].set_xlabel("X-ray current (µA)")
+        rate_ax[iax].set_ylabel("Interacting particle rate (MHz/strip)")
+        rate_ax[iax].set_title("VFAT {}".format(vfat))
+
+        efficiency_ax[iax].errorbar(true_rate/1e3, efficiency, yerr=err_efficiency, fmt=".", color="black")
+        efficiency_ax[iax].plot(
+            true_rate/1e3, ratecapability_curve(true_rate, tau), "-",
+            color="red", label=f"$\\tau\,=\,{tau*1e9:1.2f}\,\pm{err_tau*1e9:1.2f}\,ns$"
+        )
+        efficiency_ax[iax].legend()
+        efficiency_ax[iax].set_xscale("log")
+        efficiency_ax[iax].set_xlabel("Interacting particle rate (kHz/strip)")
+        efficiency_ax[iax].set_ylabel("Efficiency")
+        efficiency_ax[iax].set_title("VFAT {}".format(vfat))
+
     nvfats = 4
     nrows = int(nvfats**0.5)
     ncols = int(nvfats/nrows)
-    fig, ax = plt.subplots(nrows=nrows, ncols=ncols, figsize=(11*ncols,9*nrows))
-    ax = ax.flatten()
+    rate_fig, rate_ax = plt.subplots(nrows=nrows, ncols=ncols, figsize=(11*ncols,9*nrows))
+    rate_ax = rate_ax.flatten()
+    efficiency_fig, efficiency_ax = plt.subplots(nrows=nrows, ncols=ncols, figsize=(11*ncols,9*nrows))
+    efficiency_ax = efficiency_ax.flatten()
 
-    rate_df.groupby("vfat").apply(plot_rate)
-    fig.tight_layout()
+    rate_df.groupby("vfat").apply(analyze_rate)
+    rate_fig.tight_layout()
+    efficiency_fig.tight_layout()
 
     rate_df.to_csv(args.odir / "rate.csv", sep=";")
     if args.verbose: print("Rate csv file saved to", args.odir/"rate.csv")
-    fig.savefig(args.odir / "rate.png")
+    rate_fig.savefig(args.odir / "rate.png")
     if args.verbose: print("Rate plots saved to", args.odir/"rate.png")
+    efficiency_fig.savefig(args.odir / "efficiency.png")
+    if args.verbose: print("Efficiency plots saved to", args.odir/"efficiency.png")
 
 if __name__=="__main__": main()
